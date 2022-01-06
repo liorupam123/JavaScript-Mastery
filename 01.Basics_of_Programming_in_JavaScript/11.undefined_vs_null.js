36406c32623138248a9150364dfbd9fd7213fa1b
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:22:32 +0530

    Create 11.undefined_vs_null.js

---
+++ 01.Basics_of_Programming_in_JavaScript/11.undefined_vs_null.js
@@ -0,0 +1,23 @@
+// undefined ==> variable with no value
+// null
+
+// let firstName;
+// console.log(typeof firstName); // undefined
+// firstName = "Harshit";
+// console.log(typeof firstName, firstName); // string harshit
+
+// let myVariable = null;
+// console.log(myVariable); // null
+// myVariable = "harshit";
+// console.log(myVariable, typeof myVariable); // harshit  string
+// console.log(typeof null); // object
+// bug , error 
+
+// BigInt
+// let myNumber = BigInt(12);
+// let sameMyNumber = 123n;
+// // console.log(myNumber);
+// // console.log(Number.MAX_SAFE_INTEGER); // max interger value in javascript
+// console.log(myNumber+ sameMyNumber);
+
+// we can't add int(other data type) + BigInt
