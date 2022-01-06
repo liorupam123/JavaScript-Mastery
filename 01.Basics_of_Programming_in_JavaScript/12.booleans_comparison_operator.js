e85bd0e25728e5bbf6155ce836945b2b7ca819c1
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:23:01 +0530

    Create 12.booleans_comparison_operator.js

---
+++ 01.Basics_of_Programming_in_JavaScript/12.booleans_comparison_operator.js
@@ -0,0 +1,18 @@
+// booleans & comparison operator 
+
+// booleans 
+// true, false 
+
+// let num1 = 7;
+// let num2 = "7";
+
+// console.log(num1<num2);
+
+// == vs === 
+
+// console.log(num1 == num2); // ==> true string and int ( don't check data types , only value )
+// console.log(num1 === num2); // ==> false , (check data types , only value )
+
+// != vs !== // same difference like == vs ===
+
+// console.log(num1 !== num2);
