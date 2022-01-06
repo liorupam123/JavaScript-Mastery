c2799f4877e98b75045bcf56e93a54ade952b219
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:27:01 +0530

    Create 19.while_loop.js

---
+++ 01.Basics_of_Programming_in_JavaScript/19.while_loop.js
@@ -0,0 +1,29 @@
+// while loop 
+
+// 0 se 9 
+// dry don't repeat yourself
+let i = 0; // 1 2 3 4
+
+while(i<=9){
+    console.log(i); // 1 to 9
+    i++;
+}
+console.log(`current value of i is ${i}`); // 10
+console.log("hello");
+
+// while loop example 
+let num = 100;
+// let total = 0; //1 + 2 +3
+// let i = 0;
+
+
+// while(i<=100){
+//     total = total + i;
+//     i++;
+// }
+
+
+// console.log(total);
+
+// let total = (num*(num+1))/2;
+// console.log(total);
