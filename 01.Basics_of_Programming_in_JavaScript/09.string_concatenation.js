724163ed93b81f4d8834a985b6300c5ab7a39378
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:21:17 +0530

    Create 09.string_concatenation.js

---
+++ 01.Basics_of_Programming_in_JavaScript/09.string_concatenation.js
@@ -0,0 +1,12 @@
+// string concatenation 
+let string1 = "Rupam";
+let string2 = "Pakhira";
+
+let newString = string1 + " " +string2;
+console.log(newString); // Rupam Pakhira
+
+let string1 = "17";
+let string2 = "10";
+
+let newString = +string1 + +string2; //27
+console.log(typeof newString); // number 
