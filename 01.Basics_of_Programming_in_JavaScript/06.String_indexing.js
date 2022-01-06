d055a3a8fb1ab725f3a5a05aba3607a4385c05cc
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:19:48 +0530

    Create 06.String_indexing.js

---
+++ 01.Basics_of_Programming_in_JavaScript/06.String_indexing.js
@@ -0,0 +1,17 @@
+// String indexing 
+
+let firstName = "harshitdfjakldsfdf";
+
+//  h    a   r   s   h   i   t 
+//  0    1   2   3   4   5   6
+
+// space == string , space also adds index
+// console.log(firstName[0]);
+// length of string 
+// firstName.length 
+
+console.log(firstName.length);
+
+console.log(firstName[firstName.length-2]);
+
+// last Index : length - 1
