c750171ba1d5650939b2eb028c64f098f85518ff
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:17:45 +0530

    Create 02.Variables.js

---
+++ 01.Basics_of_Programming_in_JavaScript/02.Variables.js
@@ -0,0 +1,23 @@
+// we can declare variables in javascript without using "var"
+// when we write "use strict" , it doesn't allow that
+
+"use strict";
+
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "Harshit";
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+firstName = "Mohit";
+
+console.log(firstName);
