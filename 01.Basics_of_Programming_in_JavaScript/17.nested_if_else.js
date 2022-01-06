a7f2298d20136faed00a7ece1eda5df6cc9b4364
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:25:53 +0530

    Create 17.nested_if_else.js

---
+++ 01.Basics_of_Programming_in_JavaScript/17.nested_if_else.js
@@ -0,0 +1,66 @@
+// nested if else
+
+// winning number 19 
+
+// 19 your guess is right 
+// 17 too low 
+// 20 too high 
+
+// prompt ==> take input in string ==> +prompt == int
+
+let winningNumber = 19;
+let userGuess = +prompt("Guess a number"); // input from user
+
+if(userGuess === winningNumber){
+    console.log("Your guess is right!!");
+}else{
+    if(userGuess < winningNumber){
+        console.log("too low !!!");
+    }else{
+        console.log("too high !!!");
+    }
+}
+
+
+// if 
+// else if 
+// else if 
+// else if 
+// else 
+
+// let tempInDegree = 50;
+
+
+// if(tempInDegree < 0){
+//     console.log("extremely cold outside");
+// }else if(tempInDegree < 16){
+//     console.log("It is cold outside");
+// }else if(tempInDegree < 25){
+//     console.log("wheather is okay ");
+// }else if(tempInDegree < 35){
+//     console.log("lets go for swim");
+// }else if(tempInDegree < 45){
+//     console.log("turn on AC");
+// }else{
+//     console.log("too hot!!");
+// }
+
+// console.log("hello");
+
+// let tempInDegree = 50;
+
+// if(tempInDegree < 0){
+//     console.log("extremely cold outside");
+// }else if(tempInDegree < 16){
+//     console.log("It is cold outside");
+// }else if(tempInDegree < 25){
+//     console.log("wheather is okay ");
+// }else if(tempInDegree < 35){
+//     console.log("lets go for swim");
+// }else if(tempInDegree < 45){
+//     console.log("turn on AC");
+// }else{
+//     console.log("too hot!!");
+// }
+
+// console.log("hello there");
