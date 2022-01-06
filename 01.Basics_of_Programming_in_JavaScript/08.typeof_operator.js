a916ad1db6d57c9ccfe0aa9879a4192ec8d35856
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 00:20:50 +0530

    Create 08.typeof_operator.js

---
+++ 01.Basics_of_Programming_in_JavaScript/08.typeof_operator.js
@@ -0,0 +1,35 @@
+// typeof operator 
+
+// data types (primitive data types)
+// string "harhit"
+// number 2, 4, 5.6 
+// booleans 
+// undefined
+// null 
+// BigInt
+// Symbol
+
+// let age = 22; 
+// let firstName = "harshit";
+
+// console.log(typeof age);
+// console.log(typeof firstName);
+
+// // 22 -> "22"
+// // convert number to string. 
+// age = age + "";
+// console.log(typeof(age)); // string ("22")
+
+
+// // convert string to number. 
+
+// let myStr = +"34";
+// console.log(typeof myStr); // number
+
+// let age = "18";
+// age = Number(age);
+// console.log(typeof age); // number
+
+// let age = 18;
+// age = string(age);
+// console.log(typeof age); // string
