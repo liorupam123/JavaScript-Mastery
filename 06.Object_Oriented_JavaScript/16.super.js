8995ea80e9105c2de7048b4b9174ce04f2147ff6
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 23:54:03 +0530

    Create 16.super.js

---
+++ 06.Object_Oriented_JavaScript/16.super.js
@@ -0,0 +1,36 @@
+// super 
+class Animal {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    eat(){
+        return `${this.name} is eating`;
+    }
+
+    isSuperCute(){
+        return this.age <= 1;
+    }
+
+    isCute(){
+        return true;
+    }
+}
+
+// animal class's every objects are added in dog + adding speed
+class Dog extends Animal{
+    constructor(name, age, speed){
+				// super --> including animal's object + adding new object
+        super(name,age);
+        this.speed = speed;
+    }
+
+    run(){
+        return `${this.name} is running at ${this.speed}kmph`
+    }
+} 
+// object / instance 
+const tommy = new Dog("tommy", 3,45);
+console.log(tommy);
+console.log(tommy.run());
