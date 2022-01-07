8c82147bcf5298b8e492a48a0fe33d019600447f
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 23:10:07 +0530

    Create 01.intro_to_arrays.js

---
+++ 02.Arrays_in_JavaScript/01.intro_to_arrays.js
@@ -0,0 +1,22 @@
+// intro to arrays 
+// reference type 
+// how to create arrays
+
+// ordered collection of items 
+
+// let fruits = ["apple", "mango", "grapes"];
+// let numbers = [1,2,3,4];
+// let mixed = [1,2,2.3, "string", null, undefined];
+// console.log(mixed);
+// console.log(numbers);
+// console.log(fruits[2]);
+let fruits = ["apple", "mango", "grapes"];
+let obj = {}; // object literal
+// console.log(fruits);
+// fruits[1] = "banana"; // mango to banana
+// console.log(fruits); 
+console.log(typeof fruits); //object
+console.log(typeof obj); // object
+// checking fruits is array or not
+console.log(Array.isArray(fruits)); // true
+console.log(Array.isArray(obj)); // false
