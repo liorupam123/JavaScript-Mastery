24c24a53da9975b7d0a3e866ec81b8cd070252f2
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 23:13:44 +0530

    Create 06.const_in_array.js

---
+++ 02.Arrays_in_JavaScript/06.const_in_array.js
@@ -0,0 +1,11 @@
+// use const for creating array
+
+// adress is constant
+// heap memory ["apple", "mango"] 0x11
+
+// const fruits = ["apple", "mango"]; // 0x11
+// fruits.push("banana");
+// console.log(fruits); ==> ["apple", "mango", "banana"]
+
+// we can push/pop elemets at the end of the array but we can't change the array like
+fruits = ["a", "m"]
