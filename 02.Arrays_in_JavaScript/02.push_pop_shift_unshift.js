f026d371ccb88417ee69eb168e4f333aa660eed4
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 23:10:51 +0530

    Create 02.push_pop_shift_unshift.js

---
+++ 02.Arrays_in_JavaScript/02.push_pop_shift_unshift.js
@@ -0,0 +1,32 @@
+// array push pop 
+// array shift unshift
+
+// push and pop is > shift and unshift
+// we have to shift every array elements        first to add and delete elements at first  position
+ 
+
+let fruits = ["apple", "mango", "grapes"];
+console.log(fruits);
+
+// push ==> add elements at the beginning
+
+// fruits.push("banana");
+// console.log(fruits);
+
+// pop ==> remove elements from the last
+
+// let poppedFruit = fruits.pop();
+// console.log(fruits);
+// console.log("popped fruits is", poppedFruit);
+
+// unshift ==> add elements at the end
+
+// fruits.unshift("banana");
+// fruits.unshift("myfruit");
+// console.log(fruits);
+
+// shift ==> remove elements from the first
+
+// let removedFruit = fruits.shift();
+// console.log(fruits);
+// console.log("removed fruits is ", removedFruit);
