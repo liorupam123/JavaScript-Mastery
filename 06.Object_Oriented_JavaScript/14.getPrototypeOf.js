9450bf3235733e1cf5b76561359abb7983593ae0
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-07 23:52:13 +0530

    Create 14.getPrototypeOf.js

---
+++ 06.Object_Oriented_JavaScript/14.getPrototypeOf.js
@@ -0,0 +1,10 @@
+ let numbers = [1,2,3];
+
+// console.log(Object.getPrototypeOf(numbers));
+ console.log(Array.prototype); // constructor 
+ console.log(numbers);
+
+ function hello(){
+     console.log("hello");
+ }
+console.log(hello.prototype) // constructor : f
